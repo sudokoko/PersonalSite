fc623d7a406ba74b588863a51719d6461687893f
koko <koko@drones.gay>
2023-08-11 09:53:16 -0400

    Initial commit

---
+++ PersonalSite/Pages/Home.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sudokoko.PersonalSite.Pages;
+
+public class Home : PageModel
+{
+    public IActionResult OnGet()
+    {
+        return Page();
+    }
+}
---
+++ PersonalSite/Program.cs
@@ -0,0 +1,29 @@
+namespace Sudokoko.PersonalSite;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseWebRoot("StaticFiles");
+            webBuilder.Configure(application =>
+            {
+                application.UseForwardedHeaders();
+                application.UseStaticFiles();
+                application.UseRouting();
+                application.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapRazorPages();
+                });
+            });
+            webBuilder.ConfigureServices(services =>
+            {
+                services.AddRazorPages();
+            });
+        });
+}
